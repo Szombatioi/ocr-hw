c4bfb3d4477185c6ce856ea5f2f48bd8cf17d065
Szombati Olivér <o.szombati@gmail.com>
2026-04-24 19:04:54 +0200

    Fixing standalone issue

--- ocr_frontend/next.config.ts
+++ ocr_frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', //fixing standalone issue
 };
 
 export default nextConfig;
